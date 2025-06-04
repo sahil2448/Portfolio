d0f1be9f9e88a338efeeac61d69c88f14dd2e5bd
Sahil Kamble <syk2448@gmail.com>
2025-06-04 13:17:58 +0530

    hero image section

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ export default function App() {
   return (
     <div
       style={{
-        backgroundColor: "rgba(7, 26, 47, 0.878)",
+        backgroundColor: "rgba(7, 26, 47, 0.478)",
       }}
     >
       <BackgroundVideo />
@@ -24,19 +24,39 @@ export default function App() {
         <Hero />
         <hr className="w-[80%] mx-auto border-gray-600" />
       </section>
-      <section id="about-section">
+      <section
+        id="about-section"
+        style={{
+          backgroundColor: "rgba(7, 26, 47, 0.578)",
+        }}
+      >
         <About />
         <hr className="w-[80%] mx-auto border-gray-600" />
       </section>
-      <section id="projects-section">
+      <section
+        id="projects-section"
+        style={{
+          backgroundColor: "rgba(7, 26, 47, 0.578)",
+        }}
+      >
         <Projects />
         <hr className="w-[80%] mx-auto border-gray-600" />
       </section>
-      <section id="contact-section">
+      <section
+        id="contact-section"
+        style={{
+          backgroundColor: "rgba(7, 26, 47, 0.578)",
+        }}
+      >
         <ContactPage />
         <hr className="w-[80%] mx-auto border-gray-600" />
       </section>
-      <section id="footer-section">
+      <section
+        id="footer-section"
+        style={{
+          backgroundColor: "rgba(7, 26, 47, 0.578)",
+        }}
+      >
         <Footer />
       </section>
     </div>
--- src/Pages/Hero.jsx
+++ src/Pages/Hero.jsx
@@ -2,41 +2,55 @@ import React from "react";
 import github from "../../public/ContactIcons/github.webp";
 import gmail from "../../public/ContactIcons/gmail.webp";
 import linkedin from "../../public/ContactIcons/linkedin.webp";
+// import profile from "../../public/profile-rm-bg.png";
+import profile from "../../public/profile.jpg";
+import BackgroundVideo from "./BackgroundVideo";
 import { FaFileDownload } from "react-icons/fa";
 
 const Hero = () => {
-  let icons = [github, linkedin, gmail];
+  let icons = [
+    { imgUrl: github, lk: "https://github.com/sahil2448" },
+    {
+      imgUrl: linkedin,
+      lk: "https://www.linkedin.com/in/sahil-kamble-43b3ba279/",
+    },
+    { imgUrl: gmail, lk: "syk2448@gmail.com" },
+  ];
   return (
     <div>
+      <BackgroundVideo />
       <div
         className="hero  min-h-screen bg-none"
-        // style={{ backgroundColor: "rgba(7, 26, 47, 0.878)" }}
+        style={{ backgroundColor: "transparent" }}
       >
         <div className="hero-content flex-col lg:flex-row-reverse">
-          <div className="avatar">
-            <div className="w-72 h-96 rounded-xl">
-              {/* <img src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp" /> */}
+          <div className="avatar rounded-full flex justify-start items-start p-2 border-2 border-primary w-[70vw] sm:w-[70vw] md:w-[40vw] lg:w-[28vw] xl:w-[30vw]">
+            <div className=" rounded-full">
+              <img src={profile} className="" />
             </div>
           </div>
-          <div className="w-[60%] flex flex-col gap-4">
-            <h1 className="relative text-6xl mb-6 font-bold inline-block bg-gradient-to-r from-[#6366f1] to-[#a855f7] bg-clip-text text-transparent">
-              Hello world !, <br />
-            </h1>
-            <div className="flex flex-col py-3 mb-3  p-6">
-              <p className="text-2xl font-semibold mb-2 text-white">
+          <div className="w-[90vw] lg:w-[60%] flex flex-col justify-center items-center gap-4">
+            <div>
+              <h1 className="relative text-3xl lg:text-6xl mb-2 font-bold inline-block bg-gradient-to-r from-[#6366f1] to-[#a855f7] bg-clip-text text-transparent">
+                Hello world !
+              </h1>
+              <p className="text-xl lg:text-2xl font-semibold mb-2 text-white font-serif">
                 I’m{" "}
                 <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#6366f1] to-[#a855f7] font-extrabold">
                   Sahil Yuvraj Kamble
                 </span>
                 .
               </p>
+            </div>
+
+            <div className="flex flex-col py-3 mb-3  p-6 v-[90vw]">
               {/* <p className="text-lg text-violet-200 mb-4">
                 <span className="px-2 py-1 bg-violet-700/30 rounded-md font-medium text-violet-100">
                   Full Stack Developer
                 </span>{" "}
                 &nbsp;|&nbsp; 2nd Year @ IIT Roorkee
               </p> */}
-              <ul className="list-disc list-inside space-y-1 text-violet-100 ">
+              <ul className="list-disc list-inside space-y-1 text-violet-100 text-base ">
                 <li>
                   I'm a Passionate Full Stack Developer, problem solver &
                   upcoming 3rd year student at IIT Roorkee.
@@ -49,26 +63,39 @@ const Hero = () => {
             </div>
 
             <div className="flex flex-col gap-8">
-              <div className="flex flex-row gap-5">
-                <button className="btn btn-primary ">Projects</button>
+              <div className="flex flex-row gap-3  sm:w-full sm:gap-5">
+                <button
+                  className="btn btn-primary"
+                  onClick={() => {
+                    const element = document.getElementById("projects-section");
+                    element.scrollIntoView({ behavior: "smooth" });
+                  }}
+                >
+                  Projects
+                </button>
 
                 <button className="group btn btn-soft bg-transparent  relative overflow-hidden rounded-lg border-2 border-primary  font-semibold text-primary transition-colors duration-300 hover:text-black">
-                  <span className="absolute bottom-0 left-0 h-0 w-full bg-primary transition-all duration-300 ease-out group-hover:h-full"></span>
-                  <div className="flex justify-between items-center w-[7vw] group">
-                    <span className="relative z-10 ">My Resume</span>
-                    <FaFileDownload className="size-5 z-10 group-hover:text-black transition-all duration-200" />
-                  </div>
+                  <span className="absolute bottom-0 left-0 h-0  sm:w-full bg-primary transition-all duration-300 ease-out group-hover:h-full"></span>
+                  <a href="/Resume.pdf" download>
+                    <div className="flex justify-between items-center w-[30vw] sm:w-[7vw] group">
+                      {" "}
+                      <span className="relative z-10 ">My Resume</span>
+                      <FaFileDownload className="size-5 z-10 group-hover:text-black transition-all duration-200" />{" "}
+                    </div>
+                  </a>
                 </button>
               </div>
-              <div className="flex gap-10">
+              <div className="flex justify-center gap-5 sm:gap-10">
                 {icons.map((image, idx) => {
                   return (
-                    <img
-                      className="  size-10 shadow-md shadow-violet- rounded-lg bg-violet-700/55 hover:shadow-lg hover:shadow-violet-500 hover:bg-violet-800  transition-all duration-200  focus:outline-none focus:ring-2 focus:ring-violet-400 cursor-pointer"
-                      key={idx}
-                      src={image}
-                      alt=""
-                    />
+                    <a key={idx} href={image.lk} target="_blank">
+                      <img
+                        className=" size-8 sm:size-10 shadow-md shadow-violet- rounded-lg bg-violet-700/85 hover:shadow-lg hover:shadow-violet-500 hover:bg-violet-800  transition-all duration-200  focus:outline-none focus:ring-2 focus:ring-violet-400 cursor-pointer"
+                        key={idx}
+                        src={image.imgUrl}
+                        alt=""
+                      />
+                    </a>
                   );
                 })}
               </div>
